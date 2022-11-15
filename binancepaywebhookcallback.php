5b340490367e57c701c9295a1e720ad3763ff9c3
Enock Monyancha <enocmonyancha@gmail.com>
2022-11-15 16:00:58 +0300

    Delete binancepaywebhookcallback.php

--- binancepaywebhookcallback.php
+++
@@ -1,4 +0,0 @@
-Update Coming.
-
-This Call back will enable you to receive notifications on your web server once binance transaction is successful/complete. You can then store the information in your database
-or use it to verify if a customer has successfully completed the payment.
